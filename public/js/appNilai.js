b1bd2aebd945e825532aed0d310aaedd15f23fce
yogadahirsa <yoga@cybergl.co.id>
2023-08-26 17:17:19 +0700

    tambah stdev

--- public/js/appNilai.js
+++ public/js/appNilai.js
@@ -4,16 +4,40 @@ var env = $("#env").val();
 var bulk = [];
 var v = '';
 
+function calculateStandardDeviation(numbers) {
+    const mean = numbers.reduce((sum, num) => sum + num, 0) / numbers.length;
+    const squaredDifferences = numbers.map(num => Math.pow(num - mean, 2));
+    const variance = squaredDifferences.reduce((sum, diff) => sum + diff, 0) / numbers.length;
+    const standardDeviation = Math.sqrt(variance);
+    return standardDeviation;
+}
+
+function hitungTotal(a, b, c) {
+    var result;
+
+    const persenA = 40;
+    const persenB = 40;
+    const persenC = 20;
+
+    const maxA = 8;
+    const maxB = 8;
+    const maxC = 1;
+
+    result = ((a/maxA)*persenA)+((b/maxB)*persenB)+((c/maxC)*persenC);
+    return result.toFixed(2);
+}
+
 function prosesHitung(arr, ma, mi) {
     // console.log(arr[0].jumlah_artikel, mi, ma)
     var result = '';
+    
     let p1 = 0;
     let p2 = 0;
     let p3 = 0;
     let p4 = 0;
     let p5 = 0;
-    let art = (parseInt(arr[0].jumlah_artikel)-parseInt(mi))/(parseInt(ma)-parseInt(mi))*4;
-    console.log(art)
+    let art = (parseInt(arr[0].jumlah_artikel)-parseInt(mi))/(parseInt(ma)-parseInt(mi));
+    // console.log(art)
     arr.forEach(el => {
         const pj = JSON.parse(el.penjurian);
         for (const key in pj) {
@@ -55,7 +79,8 @@ function prosesHitung(arr, ma, mi) {
     result += `<td>${arr[0].jumlah_artikel}</td>`;
     result += `<td>${art.toFixed(2)}</td>`;
     result += `<td>${arr.length}</td>`;
-    result += `<td>${(p1+p2+p3+p4+p5+art).toFixed(2)}</td>`;
+    // result += `<td>${(p1+p2+p3+p4+p5+art).toFixed(2)}</td>`;
+    result += `<td>${hitungTotal((p1+p2),(p3+p4+p5),art)}</td>`;
     
     return result;
 }
@@ -81,6 +106,16 @@ function refreshBody(dt) {
     var tmp = '';
     const maxArtikel = _.maxBy(dt, function(o) { return o.jumlah_artikel; });
     const minArtikel = _.minBy(dt, function(o) { return o.jumlah_artikel; });
+    const liArtikel = _.map(media, 'jumlah_artikel');
+    
+    const filteredLiArtikel = _.compact(liArtikel)
+    const stdev = calculateStandardDeviation(filteredLiArtikel);
+    console.log(filteredLiArtikel)
+    console.log(stdev)
+
+    // assign min max
+    const myMax = maxArtikel.jumlah_artikel+(2*stdev);
+    const myMin = minArtikel.jumlah_artikel-(2*stdev);
     
     if (media.length) {
         media.forEach(function(el, idx) {
@@ -90,12 +125,12 @@ function refreshBody(dt) {
             const ft = _.filter(bulk, (o) => o.id == el.id);
             
             if (ft.length > 1) {
-                let a = prosesHitung(ft, maxArtikel.jumlah_artikel, minArtikel.jumlah_artikel);
+                let a = prosesHitung(ft, myMax, myMin);
                 tmp += a;   
             }
             else if (ft.length == 1) {
                 if (ft[0].penjurian) {
-                    let a = prosesHitung(ft, maxArtikel.jumlah_artikel, minArtikel.jumlah_artikel);
+                    let a = prosesHitung(ft, myMax, myMin);
                     tmp += a;
                 }
                 else {
